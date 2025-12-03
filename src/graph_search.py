1213bbec1f5f1d4176bb037da33d8fc649ae314f
Taviswhite <Taviswhite0907@gmail.com>
2025-12-02 19:07:54 -0500

    Update graph_search.py

--- src/graph_search.py
+++ src/graph_search.py
@@ -32,6 +32,27 @@ def depth_first_search(graph, start, goal):
     """
     graph.init_graph()  # Make sure all the node values are reset.
 
+    stack = [start]
+    visited = set()
+    visited.add((start.i, start.j))
+
+    graph.visited_cells.append(Cell(start.i, start.j))
+
+    while stack:
+        current = stack.pop()
+
+        if current == goal:
+            return trace_path(goal, graph)
+
+        for nbr in graph.get_neighbors(current):
+            key = (nbr.i, nbr.j)
+            if key not in visited:
+                visited.add(key)
+                graph.set_parent(nbr, current)
+                graph.visited_cells.append(Cell(nbr.i, nbr.j))
+                stack.append(nbr)
+
+
     """TODO (P3): Implement DFS (optional)."""
 
     # If no path was found, return an empty list.
@@ -46,6 +67,26 @@ def breadth_first_search(graph, start, goal):
         goal: Goal cell as a Cell object.
     """
     graph.init_graph()  # Make sure all the node values are reset.
+        
+    queue = [start]
+    visited = set()
+    visited.add((start.i, start.j))
+
+    graph.visited_cells.append(Cell(start.i, start.j))
+
+    while queue:
+        current = queue.pop(0)
+
+        if current == goal:
+            return trace_path(goal, graph)
+
+        for nbr in graph.get_neighbors(current):
+            key = (nbr.i, nbr.j)
+            if key not in visited:
+                visited.add(key)
+                graph.set_parent(nbr, current)
+                graph.visited_cells.append(Cell(nbr.i, nbr.j))
+                queue.append(nbr)
 
     """TODO (P3): Implement BFS."""
 
@@ -61,6 +102,43 @@ def a_star_search(graph, start, goal):
         goal: Goal cell as a Cell object.
     """
     graph.init_graph()  # Make sure all the node values are reset.
+   
+    # f = g + h
+    # priority queue stores (f, g, cell)
+    open_list = []
+    heapq.heappush(open_list, (0, 0, start))
+
+    graph.set_cost(start, 0)
+    visited = set()
+    visited.add((start.i, start.j))
+
+    graph.visited_cells.append(Cell(start.i, start.j))
+
+    while open_list:
+        f, g, current = heapq.heappop(open_list)
+
+        if current == goal:
+            return trace_path(goal, graph)
+
+        for nbr in graph.get_neighbors(current):
+
+            tentative_g = graph.get_cost(current) + 1
+            old_cost = graph.get_cost(nbr)
+
+            if old_cost is None or tentative_g < old_cost:
+                graph.set_cost(nbr, tentative_g)
+                h = graph.heuristic(nbr, goal)
+                f_new = tentative_g + h
+
+                graph.set_parent(nbr, current)
+
+                heapq.heappush(open_list, (f_new, tentative_g, nbr))
+
+                key = (nbr.i, nbr.j)
+                if key not in visited:
+                    visited.add(key)
+                    graph.visited_cells.append(Cell(nbr.i, nbr.j))
+
 
     """TODO (P3): Implement A*."""
 
